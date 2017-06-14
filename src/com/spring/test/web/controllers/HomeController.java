0a5bd8f59176bbf9af3c9d4e705af42a0a1960c8
Anush Poghosyan <anoushp@gmail.com>
2017-06-14 13:59:47 +0100

    added JUnit testing and logging

--- src/com/spring/test/web/controllers/HomeController.java
+++ src/com/spring/test/web/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.spring.test.web.controllers;
 
 
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+	private static Logger logger=Logger.getLogger(HomeController.class);
 	@RequestMapping("/")
 	public String showHome(){
-		
+		logger.info("showing home page");
 		return "home";
 	}
 
---
+++ test/com/spring/test/web/test/tests/UserDaoTests.java
@@ -0,0 +1,53 @@
+package com.spring.test.web.test.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.spring.test.web.dao.User;
+import com.spring.test.web.dao.UsersDao;
+
+@ActiveProfiles("dev")
+@ContextConfiguration(locations={"classpath:com/spring/test/web/config/dao-context.xml",
+"classpath:com/spring/test/web/config/security-context.xml",
+"classpath:com/spring/test/web/test/config/datasource.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserDaoTests {
+	
+	@Autowired
+	private UsersDao usersDao;
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Before
+	public void init(){
+		JdbcTemplate jdbc=new JdbcTemplate(dataSource);
+		jdbc.execute("delete from users");
+		jdbc.execute("delete from authorities");
+		
+		
+	}
+	@Test
+	public void testCreateuser(){
+		User user=new User("aaa", "hello", true, "user", "aa@tt.com");
+		
+		assertTrue("user creation should return true", usersDao.create(user));
+		List<User> users = usersDao.getAllUsers();
+		assertEquals("number of users should be 1", 1, users.size());
+		assertTrue("user should exists",usersDao.exists(user.getUsername()));
+		assertEquals("created user should be identical to retrieved user", user, users.get(0) );
+	}
+
+}
